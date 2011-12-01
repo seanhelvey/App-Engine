b8e2d1a3eac3eb6cce19aba9095c74da3270b90e
Sean Helvey <smh436@nyu.edu>
2011-12-01 16:06:57 -0500

    deployed to google app engine using sean.helvey@gmail.com

--- 1hflanevuoakfnehahcma2/hfwwgDB.py
+++ 1hflanevuoakfnehahcma2/hfwwgDB.py
@@ -3,12 +3,11 @@ from google.appengine.ext import db
 class Sighting(db.Model):
     name = db.StringProperty()
     email = db.StringProperty()
-
-    date = db.DateProperty()
-    time = db.TimeProperty()
-
+    date = db.StringProperty()
+    time = db.StringProperty()
     location = db.StringProperty()
     fin_type = db.StringProperty()
     whale_type = db.StringProperty()
     blow_type = db.StringProperty()
     wave_type = db.StringProperty()
+    which_user = db.UserProperty()
