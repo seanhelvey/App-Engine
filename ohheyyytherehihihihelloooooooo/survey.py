1b932c9409c265ba85dcb38756abe4e2d5aab4fe
Sean Helvey <smh436@nyu.edu>
2011-12-03 21:43:43 -0500

    choices and surveys stored correctly in FrontPage form

--- ohheyyytherehihihihelloooooooo/survey.py
+++ ohheyyytherehihihihelloooooooo/survey.py
@@ -12,41 +12,38 @@ cgitb.enable()
 
 class SurveyForm(djangoforms.ModelForm):                                     
     class Meta:                                                                
-        model = surveyDB.Survey
+        model = surveyDB.FrontPage
         exclude = ['choice','which_user']
 
 class SurveyInputPage(webapp.RequestHandler):
     def get(self):
         html = template.render('templates/header.html', {'title': 'SURVEY'})
         html = html + template.render('templates/form_start.html', {})
-        surveys = db.GqlQuery("SELECT * FROM Survey")
+        surveys = db.GqlQuery("SELECT * FROM FrontPage WHERE name != ''")
         html = html +  "<h2>Do you want to take one of these surveys?<br></h2>"
 
         for survey in surveys:
-            html = html + "<INPUT TYPE=RADIO NAME='SurveyInputPage' VALUE="
-            html = html + str(survey.question) + "> %s <br>" %survey.question + "<br>"
+            x = survey.name
+            x = x.replace(" ","_")
+            html = html + "<INPUT TYPE=RADIO NAME='choice' VALUE=" + x + "> %s" %survey.name + "<br>"
             
-        html = html +  "<h2>Or would you like to make your own survey??<br></h2>"
+        html = html +  "<h2>Or would you like to make your own survey?<br></h2>"
         html = html + str(SurveyForm(auto_id=False))
         html = html + template.render('templates/form_end.html', {'sub_title': 'Submit'})
         html = html + template.render('templates/footer.html', {'links': ''})
         self.response.out.write(html)
 
     def post(self): 
-        new_survey = surveyDB.Survey()
         
-        if(self.request.get('question') == ''):
-            new_survey.choice = self.request.get('choice')
-
-        else:
-            new_survey.question = self.request.get('question')
-            new_survey.answerOne = self.request.get('answerOne')
-            new_survey.answerTwo = self.request.get('answerTwo')
-            new_survey.answerThree = self.request.get('answerThree')
-        
-        new_survey.which_user = users.get_current_user()
-
-        new_survey.put()
+        front_page = surveyDB.FrontPage()
+        front_page.name = self.request.get('name')
+        front_page.question = self.request.get('question')
+        front_page.answerOne = self.request.get('answerOne')
+        front_page.answerTwo = self.request.get('answerTwo')
+        front_page.answerThree = self.request.get('answerThree')
+        front_page.choice = self.request.get('choice')
+        front_page.which_user = users.get_current_user()
+        front_page.put()
         
         html = template.render('templates/header.html', {'title': 'Thank you!'})
         html = html + template.render('templates/footer.html',
